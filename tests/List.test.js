668d5d4d3b0760e649f31b3b2bdb4830416a670b
The Jared Wilcurt <TheJaredWilcurt@users.noreply.github.com>
2020-01-26 14:22:12 -0500

    Add helpers

--- tests/List.test.js
+++ tests/List.test.js
@@ -1,8 +1,12 @@
+const helpers = require('./helpers.js');
+
 import { shallowMount } from '@vue/test-utils';
 import List from './components/List.vue';
 
 describe('List.vue', () => {
   test('Snapshot unchanged', () => {
+    helpers.mockSettings({});
+
     const wrapper = shallowMount(List, {
       propsData: {
         items: ['one', 'two', 'three']
