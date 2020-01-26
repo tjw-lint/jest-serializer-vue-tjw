3ce33659a983bd2d0d752ab828d68abfd5f9db46
The Jared Wilcurt <TheJaredWilcurt@users.noreply.github.com>
2020-01-26 13:22:14 -0500

    data-test/server-rendered/qa tests

--- tests/DataTestIds.test.js
+++ tests/DataTestIds.test.js
@@ -1,8 +1,87 @@
+const helpers = require('./helpers.js');
+
 import { shallowMount } from '@vue/test-utils';
 import DataTestIds from './components/DataTestIds.vue';
 
 describe('DataTestIds.vue', () => {
-  test('Snapshot unchanged', () => {
+  test('Remove data-server-rendered, data-test, data-testid, data-test-id by default', () => {
+    helpers.mockSettings({});
+
+    const wrapper = shallowMount(DataTestIds);
+
+    expect(wrapper.html())
+      .toMatchSnapshot();
+  });
+
+  test('Only data-server-rendered removed', () => {
+    helpers.mockSettings({
+      removeServerRendered: true,
+      removeDataTest: false,
+      removeDataTestid: false,
+      removeDataTestId: false,
+      removeDataQa: false
+    });
+
+    const wrapper = shallowMount(DataTestIds);
+
+    expect(wrapper.html())
+      .toMatchSnapshot();
+  });
+
+  test('Only data-test removed', () => {
+    helpers.mockSettings({
+      removeServerRendered: false,
+      removeDataTest: true,
+      removeDataTestid: false,
+      removeDataTestId: false,
+      removeDataQa: false
+    });
+
+    const wrapper = shallowMount(DataTestIds);
+
+    expect(wrapper.html())
+      .toMatchSnapshot();
+  });
+
+  test('Only data-testid removed', () => {
+    helpers.mockSettings({
+      removeServerRendered: false,
+      removeDataTest: false,
+      removeDataTestid: true,
+      removeDataTestId: false,
+      removeDataQa: false
+    });
+
+    const wrapper = shallowMount(DataTestIds);
+
+    expect(wrapper.html())
+      .toMatchSnapshot();
+  });
+
+  test('Only data-test-id removed', () => {
+    helpers.mockSettings({
+      removeServerRendered: false,
+      removeDataTest: false,
+      removeDataTestid: false,
+      removeDataTestId: true,
+      removeDataQa: false
+    });
+
+    const wrapper = shallowMount(DataTestIds);
+
+    expect(wrapper.html())
+      .toMatchSnapshot();
+  });
+
+  test('Only data-qa removed', () => {
+    helpers.mockSettings({
+      removeServerRendered: false,
+      removeDataTest: false,
+      removeDataTestid: false,
+      removeDataTestId: false,
+      removeDataQa: true
+    });
+
     const wrapper = shallowMount(DataTestIds);
 
     expect(wrapper.html())
