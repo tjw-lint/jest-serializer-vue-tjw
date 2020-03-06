c75d90c249ebd5f61b10831b6d93478500ebccb8
The Jared Wilcurt <TheJaredWilcurt@users.noreply.github.com>
2020-03-06 00:52:00 -0500

    logic

--- src/addInputValues.js
+++ src/addInputValues.js
@@ -1,20 +1,50 @@
 const helpers = require('./helpers.js');
 
 /**
- * Sets the attribute on element based on domProps.
+ * Sets the value attribute on element based on data from the vnode.
+ * If a value already exists, like with radio dials, set a v-model value.
  *
- * @param {object} vnode     A Vue node
- * @param {string} attribute 'value' or 'checked'
+ * @param {object} vnode      A Vue node
+ * @param {string} attribute  'value' or 'checked'
  */
-function setAttribute (vnode, attribute) {
+function setValue (vnode, attribute) {
+  vnode.data = vnode.data || {};
+  vnode.data.attrs = vnode.data.attrs || {};
+  let key = 'If the vnode value is `undefined` then this key won\'t match at the end.';
+  let value = key;
+
+  // Select, radio, and checkboxes use this
   if (
-    vnode.data &&
+    vnode.data.directives &&
+    vnode.data.directives.length
+  ) {
+    const directive = vnode.data.directives.find(function (directive) {
+      return directive.rawName === 'v-model';
+    });
+    if (directive && directive.hasOwnProperty('value')) {
+      value = directive.value;
+    }
+  }
+
+  // should work on text, number, range, date, and textarea
+  if (
     vnode.data.domProps &&
     vnode.data.domProps.hasOwnProperty(attribute)
   ) {
-    let value = vnode.data.domProps[attribute];
-    vnode.data.attrs = vnode.data.attrs || {};
-    vnode.data.attrs.value = helpers.swapQuotes(helpers.stringify(value));
+    value = vnode.data.domProps[attribute];
+  }
+
+  // this allows us to apply value="undefined" or v-model="undefined"
+  // but only in the cases where that was the value in the vnode
+  if (value === key) {
+    return;
+  }
+
+  const processed = helpers.swapQuotes(helpers.stringify(value));
+  if (!vnode.data.attrs.value) {
+    vnode.data.attrs.value = processed;
+  } else {
+    vnode.data.attrs['v-model'] = processed;
   }
 }
 
@@ -25,8 +55,7 @@ function setAttribute (vnode, attribute) {
  * @param {object} vnode A Vue node
  */
 function addVnodeValueAttribute (vnode) {
-  setAttribute(vnode, 'value');
-  setAttribute(vnode, 'checked');
+  setValue(vnode, 'value');
 
   if (vnode.children) {
     vnode.children.forEach(function (childVNode) {
