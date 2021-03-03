8b08c5c64702d46650b512edd13341b00139af6c
The Jared Wilcurt <4629794+TheJaredWilcurt@users.noreply.github.com>
2021-03-03 17:16:45 -0500

    fixed tests

--- tests/Istanbul.test.js
+++ tests/Istanbul.test.js
@@ -4,16 +4,6 @@ import { mount } from '@vue/test-utils';
 import Instanbul from './components/Instanbul.vue';
 
 describe('Instanbul.vue', () => {
-  test('Comment remains', () => {
-    helpers.mockSettings({
-      removeIstanbulComments: false
-    });
-    let wrapper = mount(Instanbul);
-
-    expect(wrapper)
-      .toMatchSnapshot();
-  });
-
   test('Comment removed', () => {
     helpers.mockSettings({
       removeIstanbulComments: true
